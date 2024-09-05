37ed04ff91935958e32cd882f4f83b235e8a6f6f
fardardnsyh <faridardiansyah061@gmail.com>
2024-09-05 21:09:07 +0700

    Files upload

---
+++ content/pages/contact.md
@@ -0,0 +1,7 @@
+---
+title: "Contact Me"
+template: "page"
+socialImage:
+---
+
+My social links are located in the footer below or feel free to reach out through email jeremy dot kreutzbender at gmail dot com.
---
+++ src/hooks/use-site-metadata.js
@@ -0,0 +1,41 @@
+// @flow strict
+import { useStaticQuery, graphql } from 'gatsby';
+
+const useSiteMetadata = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query SiteMetaData {
+        site {
+          siteMetadata {
+            author {
+              name
+              bio
+              photo
+              contacts {
+                linkedin
+                github
+                twitter
+                stackoverflow
+                instagram
+                email
+              }
+            }
+            menu {
+              label
+              path
+            }
+            url
+            title
+            subtitle
+            copyright
+            disqusShortname
+          }
+        }
+      }
+    `
+  );
+
+  return site.siteMetadata;
+};
+
+export default useSiteMetadata;
